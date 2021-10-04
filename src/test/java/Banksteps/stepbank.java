f66a716daf6167ca6fa4afb585cc25e11da4467c
ArchBN <archana.narayana@gmail.com>
2021-10-04 12:58:34 +0530

    Cucumber Datadriven approach

--- src/test/java/Banksteps/stepbank.java
+++ src/test/java/Banksteps/stepbank.java
@@ -3,23 +3,24 @@ package Banksteps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class stepbank {
     WebDriver driver;
-
     @Given("Open DemoGuru URL and launch the application")
     public void open_demo_guru_url_and_launch_the_application() {
-       System.setProperty("webdriver.chrome.driver", "C://Users//archana//chromedriver_win32//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C://Users//archana//chromedriver_win32//chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         String baseUrl = "http://www.demo.guru99.com/V4/";
         driver.get(baseUrl);
+
     }
     @When("^User enters \"([^\"]*)\" and \"([^\"]*)\"$")
-    public void userEntersUsername(String username,String password) {
+    public void userEntersUsername(String username, String password) {
         {
             driver.findElement(By.name("uid")).clear();
             driver.findElement(By.name("uid")).sendKeys(username);
@@ -29,12 +30,19 @@ public class stepbank {
         }
     }
     @Then("^Submit the credentials$")
-    public void submitTheCredentials() {
+    public void submitTheCredentials()
+    {
         driver.findElement(By.name("btnLogin")).click();
-        driver.quit();
+    }
+    @Then("^Homepage should be displayed$")
+    public void homepageShouldBeDisplayed()
+    {
+        String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(EXPECT_TITLE,actualTitle);
+                System.out.println("Login successful:Passed");
+                    driver.close();
     }
 
 
 }
-
-
--- src/test/resources/NewFeatures/Banklogin.feature
+++ src/test/resources/NewFeatures/Banklogin.feature
@@ -3,13 +3,12 @@
    # Given Open DemoGuru URL and launch the application
     #When User enters username and password
     #Then Submit the credentials
-
-
 Feature:Bank application login
   Scenario Outline:Validate Login page of bank
     Given Open DemoGuru URL and launch the application
     When User enters "<username>" and "<password>"
     Then Submit the credentials
+    Then Homepage should be displayed
     Examples:
       | username   | password |
       | mngr356926 | Yheruje  |
